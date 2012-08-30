2965d5eaac4e69427fc97f5074a2932ba6a490fd
radem <richy486@gmail.com>
2012-08-30 14:08:29 +1000

    - first commit

---
+++ Cocos2dSkeleton/GameLayer.h
@@ -0,0 +1,15 @@
+//
+//  GameLayer.h
+//  Cocos2dSkeleton
+//
+//  Created by Richard Adem on 26/08/12.
+//
+//
+
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer
+
++(CCScene *) scene;
+
+@end
